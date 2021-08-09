bbbbb0269bc267a3e60218a4d64a474f24c84e74
domhall <dhall@worth.systems>
2021-08-09 16:14:32 +0100

    Added secret keys for terraform

--- github/main.tf
+++ github/main.tf
@@ -18,3 +18,13 @@ resource "github_repository" "turtles" {
 
   visibility = "public"
 }
+resource "github_actions_secret" "aws_key" {
+  repository       = github_repository.turtles.name
+  secret_name      = "aws_key"
+  plaintext_value  = var.turtle_access_key
+}
+resource "github_actions_secret" "aws_secret_key" {
+  repository       = github_repository.turtles.name
+  secret_name      = "aws_secret_key"
+  plaintext_value  = var.turtle_secret_access_key
+}
--- github/variables.tf
+++ github/variables.tf
@@ -1,3 +1,9 @@
 variable "github_token" {
   type = string
 }
+variable "turtle_access_key" {
+  type = string
+}
+variable "turtle_secret_access_key" {
+  type = string
+}
